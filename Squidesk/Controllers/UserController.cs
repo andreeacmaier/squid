22a14761ec144f403e530264e1e988bfca252692
andreea-cornelia maier <andreeacmaier@gmail.com>
2019-05-05 22:39:19 +0300

    Login

---
+++ Squidesk/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Squidesk.Models;
+using System.Data.SqlClient;
+
+namespace Squidesk.Controllers
+{
+    public class UserController : Controller
+    {
+        SqlConnection conn = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+
+        // GET: User
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Verify(User user)
+        {
+            connectionString();
+            conn.Open();
+            com.Connection = conn;
+            com.CommandText = "select * from Users where username='"+user.username+"' and parola='"+user.parola+"' ";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                conn.Close();
+                return View("LoggedIn");
+            }
+            else
+            {
+                conn.Close();
+                return View("Error");
+            }
+            
+        }
+
+        private void connectionString()
+        {
+            conn.ConnectionString = "Data Source=LAPTOP-BQ5NVT98\\SQLEXPRESS;Initial Catalog=Squidesk;Integrated Security=True;";
+        }
+    }
+}
---
+++ Squidesk/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Squidesk.Models
+{
+    public class User
+    {
+
+        public String username { get; set; }
+        public String nume { get; set; }
+        public String prenume { get; set; }
+        public String parola { get; set; }
+        public int nrMat { get; set; }
+
+        
+    }
+}
